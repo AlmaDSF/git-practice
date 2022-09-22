07456d32841cee3ca9224356eede38652b2a6d45
Jessica <jessicaambrizmadrigal@gmail.com>
2022-09-22 14:16:57 -0700

    modifed a + c

--- function_a.py
+++ function_a.py
@@ -17,9 +17,6 @@ def most_common_value(number_list):
 
     return most_common_value
 
-    return most_common_value
-
-
 if __name__ == "__main__":
     nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
     print(f"Most common value = {most_common_value(nums)}")
